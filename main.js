5128de81035771a17cacb776d34df7d57f246a74
tinachen <wanting@byui.edu>
2024-06-22 17:56:56 -0600

    88999

---
+++ main.js
@@ -0,0 +1,11 @@
+// function checkScreenSize() {
+//     const description = document.querySelector('.description');
+//     if (window.innerWidth <= 650 && description) {
+//         description.style.display = 'none';
+//     } else if (window.innerWidth > 650 && description) {
+//         description.style.display = 'block';
+//     }
+// }
+
+// window.addEventListener('resize', checkScreenSize);
+// window.addEventListener('load', checkScreenSize);
